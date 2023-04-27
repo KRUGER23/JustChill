529de13f8cb95e982113e07015cd22e9c49e171b
KRUGER23 <131855281+KRUGER23@users.noreply.github.com>
2023-04-27 23:07:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,73 +1,86 @@
-# Создаем пустое поле 3x3
-board = [[' ', ' ', ' '],
-         [' ', ' ', ' '],
-         [' ', ' ', ' ']]
+def greet():
+    print("-------------------")
+    print("  Приветсвуем вас  ")
+    print("      в игре       ")
+    print("  крестики-нолики  ")
+    print("-------------------")
+    print(" формат ввода: x y ")
+    print(" x - номер строки  ")
+    print(" y - номер столбца ")
 
-# Определяем функцию для отрисовки игрового поля
-def print_board(board):
-    print("-------------")
-    for row in board:
-        print("| " + " | ".join(row) + " |")
-        print("-------------")
+def show():
+    print()
+    print("    | 0 | 1 | 2 | ")
+    print("  --------------- ")
+    for i, row in enumerate(field):
+        row_str = f"  {i} | {' | '.join(row)} | "
+        print(row_str)
+        print("  --------------- ")
+    print()
 
-# Определяем функцию для проверки победы
-def check_win(board, player):
-    # Проверяем строки
-    for row in board:
-        if row.count(player) == 3:
+def ask():
+    while True:
+        cords = input("         Ваш ход: ").split()
+        
+        if len(cords) != 2:
+            print(" Введите 2 координаты! ")
+            continue
+        
+        x, y = cords
+        
+        if not(x.isdigit()) or not(y.isdigit()):
+            print(" Введите числа! ")
+            continue
+        
+        x, y = int(x), int(y)
+        
+        if 0 > x or x > 2 or  0 > y or  y > 2 :
+            print(" Координаты вне диапазона! ")
+            continue
+        
+        if field[x][y] != " ":
+            print(" Клетка занята! ")
+            continue
+        
+        return x, y
+            
+def check_win():
+    win_cord = (((0, 0), (0, 1), (0, 2)), ((1, 0), (1, 1), (1, 2)), ((2, 0), (2, 1), (2, 2)),
+                ((0, 2), (1, 1), (2, 0)), ((0, 0), (1, 1), (2, 2)), ((0, 0), (1, 0), (2, 0)),
+                ((0, 1), (1, 1), (2, 1)), ((0, 2), (1, 2), (2, 2)))
+    for cord in win_cord:
+        symbols = []
+        for c in cord:
+            symbols.append(field[c[0]][c[1]])
+        if symbols == ["X", "X", "X"]:
+            print("Выиграл X!!!")
             return True
-
-    # Проверяем столбцы
-    for col in range(3):
-        if board[0][col] == player and board[1][col] == player and board[2][col] == player:
+        if symbols == ["0", "0", "0"]:
+            print("Выиграл 0!!!")
             return True
-
-    # Проверяем диагонали
-    if board[0][0] == player and board[1][1] == player and board[2][2] == player:
-        return True
-    if board[0][2] == player and board[1][1] == player and board[2][0] == player:
-        return True
-
-    # Если ни один из условий не выполнен, возвращаем False
     return False
 
-# Определяем функцию для хода игрока
-def player_move(board, player):
-    # Получаем координаты клетки, куда игрок хочет поставить свой знак
-    row = int(input("Введите номер строки (1-3): ")) - 1
-    col = int(input("Введите номер столбца (1-3): ")) - 1
-
-    # Проверяем, что клетка свободна
-    if board[row][col] != ' ':
-        print("Эта клетка уже занята. Попробуйте еще раз.")
-        player_move(board, player)
+greet()
+field = [[" "] * 3 for i in range(3) ]
+count = 0
+while True:
+    count += 1
+    show()
+    if count % 2 == 1:
+        print(" Ходит крестик!")
     else:
-        # Ставим знак игрока на выбранную клетку
-        board[row][col] = player
-
-# Определяем основную функцию игры
-def main():
-    print("Добро пожаловать в игру крестики-нолики!")
-    print_board(board)
-
-    # Цикл поочередных ходов игроков
-    for turn in range(9):
-        if turn % 2 == 0:
-            player = 'X'
-        else:
-            player = 'O'
-
-        print(f"Ходит игрок {player}")
-        player_move(board, player)
-        print_board(board)
-
-        # Проверяем, есть ли победитель
-        if check_win(board, player):
-            print(f"Игрок {player} победил!")
-            return
-
-    # Если все клетки заняты и нет победителя, объявляем ничью
-    print("Ничья!")
-
-# Запускаем игру
-main()
+        print(" Ходит нолик!")
+    
+    x, y = ask()
+    
+    if count % 2 == 1:
+        field[x][y] = "X"
+    else:
+        field[x][y] = "0"
+    
+    if check_win():
+        break
+    
+    if count == 9:
+        print(" Ничья!")
+        break
